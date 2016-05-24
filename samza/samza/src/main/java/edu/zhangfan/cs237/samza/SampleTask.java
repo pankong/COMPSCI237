c91c2a1c32592b2bfda5d459d789a1765f1cda1a
Dong Zhangfan <dong.zhangfan@gmail.com>
2016-05-24 11:10:27 -0700

    Introduce Samza processing module into this repository, which includes:
    1. source code of samza processing layer and simulated producer.
    2. readme.org elaborate how to build and run these tools.
    3. independent gitignore file.

---
+++ samza/samza/src/main/java/edu/zhangfan/cs237/samza/SampleTask.java
@@ -0,0 +1,21 @@
+package edu.zhangfan.cs237.samza;
+
+import org.apache.samza.system.IncomingMessageEnvelope;
+import org.apache.samza.system.OutgoingMessageEnvelope;
+import org.apache.samza.system.SystemStream;
+import org.apache.samza.task.MessageCollector;
+import org.apache.samza.task.StreamTask;
+import org.apache.samza.task.TaskCoordinator;
+
+/**
+ * This sample task is for testing our samza workflow.
+ * It simplify forward the message to a stream named "forward"
+ */
+public class SampleTask implements StreamTask {
+  private static final SystemStream OUTPUT_STREAM = new SystemStream("kafka", "forward");
+
+  public void process(IncomingMessageEnvelope incomingMessageEnvelope, MessageCollector messageCollector, TaskCoordinator taskCoordinator) throws Exception {
+    String message = (String) incomingMessageEnvelope.getMessage();
+    messageCollector.send(new OutgoingMessageEnvelope(OUTPUT_STREAM, message));
+  }
+}
